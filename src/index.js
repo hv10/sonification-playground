13bd1c647e78a482be6df3db3f1881cbc82a22d6
hv10 <noeldanz@gmail.com>
2021-01-01 17:08:41 +0100

    persistence

--- src/index.js
+++ src/index.js
@@ -3,92 +3,69 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { configureStore } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  getDefaultMiddleware,
+  combineReducers,
+} from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  autoMergeLevel2,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 import { ToneJSContext } from "./ToneJSContext";
 import { ViewerContext } from "./ViewerContext";
 
 import nodeReducer from "./reducer/nodeReducer";
 import edgeReducer from "./reducer/edgeReducer";
 import { dataviewReducer } from "./reducer/dataViewReducer";
-const initialState = {
-  nodes: [
-    {
-      id: "3",
-      targetPosition: "left",
-      sourcePosition: "right",
-      type: "audioOut",
-      data: {
-        label: "Audio Label 1",
-        onMuteToggle: (e) => {
-          console.log(e);
-        },
-        id: "3",
-      },
-      position: { x: 0, y: 200 },
-    },
-    {
-      id: "5",
-      targetPosition: "left",
-      sourcePosition: "right",
-      type: "graphNode",
-      data: {
-        label: "Graph Label 1",
-        onChange: (event) => {
-          console.log(event.target.value);
-        },
-        id: "5",
-      },
-      position: { x: 150, y: 200 },
-    },
-    {
-      id: "8",
-      targetPosition: "left",
-      sourcePosition: "right",
-      type: "csvInput",
-      data: {
-        label: "CSV Label 1",
-        onChange: (event) => {
-          console.log(event.target.value);
-        },
-        id: "8",
-      },
-      position: { x: 300, y: 200 },
-    },
-    {
-      id: "4",
-      targetPosition: "left",
-      sourcePosition: "right",
-      type: "synthNode",
-      data: {
-        label: "Synth Label 1",
-        onChange: (event) => {
-          console.log(event.target.value);
-        },
-        id: "4",
-      },
-      position: { x: 450, y: 200 },
-    },
-  ],
+import { Loading } from "carbon-components-react";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  stateReconciler: autoMergeLevel2,
 };
-const reducer = {
+
+const reducer = combineReducers({
   nodes: nodeReducer,
   edges: edgeReducer,
   dataviews: dataviewReducer,
-};
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 const store = configureStore({
-  reducer,
+  reducer: persistedReducer,
   devTools: process.env.NODE_ENV !== "production",
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
   //preloadedState: initialState,
 });
+
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <ToneJSContext.Provider value={{}}>
       <ViewerContext.Provider value={{}}>
         <Provider store={store}>
-          <App />
+          <PersistGate loading={<Loading active />} persistor={persistor}>
+            <App />
+          </PersistGate>
         </Provider>
       </ViewerContext.Provider>
     </ToneJSContext.Provider>
