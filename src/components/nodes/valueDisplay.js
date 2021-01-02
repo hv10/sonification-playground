acb03f434c83fd2b0e4fee37b5254afa0ab9fdc9
hv10 <noeldanz@gmail.com>
2021-01-02 02:40:01 +0100

    fix value display

--- src/components/nodes/valueDisplay.js
+++ src/components/nodes/valueDisplay.js
@@ -9,7 +9,17 @@ import ViewerContext from "../../ViewerContext";
 import ToneJSContext from "../../ToneJSContext";
 import * as Tone from "tone";
 
-const ValueDisplay = ({ name, value }) => {
+const ValueDisplay = ({ name, updateValue = () => {} }) => {
+  const [value, setValue] = React.useState(null);
+  const collectValue = () => {
+    setValue(updateValue);
+  };
+  React.useEffect(() => {
+    const intv = setInterval(collectValue, 150);
+    return () => {
+      clearInterval(intv);
+    };
+  }, []);
   return (
     <Tile className="viewerTile">
       <span>{name}</span>
@@ -25,15 +35,22 @@ const ValueDisplayNode = ({ data }) => {
   const [current, setCurrent] = React.useState(null);
   const updateValue = () => {
     if (toneJSContext[data.id]) {
-      setCurrent(toneJSContext[data.id].dataIn.getValue()[0].toFixed(5));
+      const val = toneJSContext[data.id].dataIn.getValue().toFixed(5);
+      setCurrent(val);
+      return val;
     } else {
       setCurrent(null);
+      return null;
     }
   };
   React.useEffect(() => {
     if (viewerContext[data.id])
       viewerContext[data.id].renderComponent = (
-        <ValueDisplay name={data.label} value={current} />
+        <ValueDisplay
+          name={data.label}
+          value={current}
+          updateValue={updateValue}
+        />
       );
   }, [current]);
   React.useEffect(() => {
@@ -41,13 +58,19 @@ const ValueDisplayNode = ({ data }) => {
       viewerContext[data.id] = {
         id: data.id,
         gridData: { x: 0, y: 0, w: 2, h: 1, isResizable: false },
-        renderComponent: <ValueDisplay name={data.label} value={current} />,
+        renderComponent: (
+          <ValueDisplay
+            name={data.label}
+            value={current}
+            updateValue={updateValue}
+          />
+        ),
       };
     }
     if (!toneJSContext[data.id]) {
       toneJSContext[data.id] = {
-        dataIn: new Tone.Analyser({
-          type: "waveform",
+        dataIn: new Tone.Meter({
+          normalRange: true,
         }),
       };
     }
